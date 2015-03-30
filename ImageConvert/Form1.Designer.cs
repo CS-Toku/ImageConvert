0c6fee0cca55026f5979e2f4191bf74d5014efd6
cs_toku@sce-toku.jp <cs_toku@sce-toku.jp>
2015-03-30 22:37:39 +0900

    Ver.1.0.0

---
+++ ImageConvert/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace ImageConvert
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog2 = new System.Windows.Forms.FolderBrowserDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ファイルを追加";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(640, 101);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(599, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Type";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 397);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(581, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(640, 127);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(121, 19);
+            this.numericUpDown1.TabIndex = 4;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(640, 152);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(121, 19);
+            this.numericUpDown2.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(599, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Width";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(599, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Height";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(601, 397);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(160, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Convert";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(13, 60);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(580, 309);
+            this.listView1.TabIndex = 9;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(102, 31);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(83, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "フォルダを追加";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(518, 31);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "削除";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MS UI Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label4.Location = new System.Drawing.Point(12, 372);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "label4";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(601, 368);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(160, 23);
+            this.button5.TabIndex = 13;
+            this.button5.Text = "出力フォルダを選択";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(773, 432);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog2;
+    }
+}
+
---
+++ ImageConvert/Form1.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageConvert
+{
+
+    public partial class Form1 : Form
+    {
+        public static string[] type = { ".jpeg", ".jpg", ".png", ".bmp", ".gif" };
+        public Dictionary<string, System.Drawing.Imaging.ImageFormat> ImageType;
+        public string SavePath;
+        public System.Threading.Thread ConvThread;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //リストビュー初期化
+            listView1.View = View.Details;
+            listView1.Items.Clear();
+            listView1.Columns.Clear();
+
+            //項目1を追加
+            ColumnHeader ColumnH1 = new ColumnHeader();
+            ColumnH1.Text = "Title";
+            ColumnH1.Width = 100;
+            listView1.Columns.Add(ColumnH1);
+
+            //項目2を追加
+            ColumnHeader ColumnH2 = new ColumnHeader();
+            ColumnH2.Text = "Width";
+            ColumnH2.Width = 150;
+            listView1.Columns.Add(ColumnH2);
+
+            //項目3を追加
+            ColumnHeader ColumnH3 = new ColumnHeader();
+            ColumnH3.Text = "Height";
+            ColumnH3.Width = 100;
+            listView1.Columns.Add(ColumnH3);
+
+            numericUpDown1.Minimum = 1;
+            numericUpDown2.Minimum = 1;
+            numericUpDown1.Maximum = 10000;
+            numericUpDown2.Maximum = 10000;
+
+            listView1.CheckBoxes = true;
+            listView1.FullRowSelect = true;
+
+            openFileDialog1.Multiselect = true;
+            openFileDialog1.Filter = "画像ファイル|*.bmp;*.gif;*.png;*.jpeg;*.jpg";
+
+            ImageType = new Dictionary<string, System.Drawing.Imaging.ImageFormat>();
+            comboBox1.Items.Add("JPG");
+            ImageType["JPG"] = System.Drawing.Imaging.ImageFormat.Jpeg;
+            comboBox1.Items.Add("PNG");
+            ImageType["PNG"] = System.Drawing.Imaging.ImageFormat.Png;
+            comboBox1.Items.Add("BMP");
+            ImageType["BMP"] = System.Drawing.Imaging.ImageFormat.Bmp;
+            comboBox1.Items.Add("GIF");
+            ImageType["GIF"] = System.Drawing.Imaging.ImageFormat.Gif;
+
+            comboBox1.SelectedIndex = 0;
+
+            label4.Text = System.IO.Directory.GetCurrentDirectory() + @"\output";
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                // 選択されたファイル名を格納
+                foreach (string path in openFileDialog1.FileNames)
+                {
+                    if (Array.IndexOf(type, Path.GetExtension(path).ToLower()) >= 0)
+                    {
+                        listView1.Items.Add(new ImageRow(path));
+                    }
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                // 選択されたファイル名を格納
+                string[] files = System.IO.Directory.GetFiles(folderBrowserDialog1.SelectedPath);
+                foreach (string file in files)
+                {
+                    if (Array.IndexOf(type, Path.GetExtension(file).ToLower()) >= 0)
+                    {
+                        listView1.Items.Add(new ImageRow(file));
+                    }
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (ConvThread == null)
+            {
+                ConvThread =
+                    new System.Threading.Thread(
+                    new System.Threading.ThreadStart(conv_Method));
+                ConvThread.IsBackground = true;
+                ConvThread.Start();
+                button2.Text = "Abort";
+            }
+            else if (ConvThread.IsAlive == false)
+            {
+                ConvThread =
+                    new System.Threading.Thread(
+                    new System.Threading.ThreadStart(conv_Method));
+                ConvThread.IsBackground = true;
+                ConvThread.Start();
+                button2.Text = "Abort";
+            }
+            else if (ConvThread.IsAlive == true)
+            {
+                ConvThread.Abort();
+                ConvThread.Join();
+                button2.Text = "Convert";
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            foreach (ListViewItem item in listView1.SelectedItems)
+            {
+                listView1.Items.Remove(item);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                label4.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+
+        delegate string GetFileTypeDelegate();
+        private string GetFileType()
+        {
+            return comboBox1.Text;
+        }
+
+        delegate void ProgressBarInitDelegate(int min, int max);
+        private void ProgressBarInit(int min, int max)
+        {
+            progressBar1.Minimum = min;
+            progressBar1.Maximum = max;
+            progressBar1.Value = 0;
+        }
+
+        delegate ImageRow GetListItemDelegate(int index);
+        private ImageRow GetListItem(int index)
+        {
+            return (ImageRow)listView1.Items[index];
+        }
+
+        delegate bool GetStatusDelegate(ImageRow ir);
+        private bool GetStatus(ImageRow ir)
+        {
+            return ir.Checked;
+        }
+
+        delegate void SetStatusDelegate(ImageRow ir, bool b);
+        private void SetStatus(ImageRow ir, bool b)
+        {
+            ir.Checked = b;
+        }
+
+        delegate void SetProgressBarValueDelegate(int num);
+        private void SetProgressBarValue(int num)
+        {
+            progressBar1.Value = num;
+        }
+
+        delegate void SetButtonTextDelegate(string str);
+        private void SetButtonText(string str)
+        {
+            button2.Text = str;
+        }
+
+        private void conv_Method()
+        {
+            string res = "Complete!!";
+            try
+            {
+                int size = listView1.Items.Count;
+                int width = (int)numericUpDown1.Value;
+                int height = (int)numericUpDown2.Value;
+                string imagetype_str = (string)Invoke(new GetFileTypeDelegate(GetFileType));
+                System.Drawing.Imaging.ImageFormat imagetype = ImageType[imagetype_str];
+                string ext = "." + imagetype_str.ToLower();
+                Invoke(new ProgressBarInitDelegate(ProgressBarInit), new Object[] { 0, size });
+
+                for (int i = 0; i < size; i++)
+                {
+                    ImageRow item = (ImageRow)Invoke(new GetListItemDelegate(GetListItem), new Object[] { i });
+                    bool Checked = (bool)Invoke(new GetStatusDelegate(GetStatus), new Object[] { item });
+                    if (Checked == true)
+                    {
+                        Bitmap bmp = new Bitmap(item.PathName);
+                        Bitmap rbmp = new Bitmap(width, height);
+
+                        Graphics g = Graphics.FromImage(rbmp);
+                        g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+                        g.DrawImage(bmp, 0, 0, width, height);
+                        g.Dispose();
+
+                        string filename = System.IO.Path.GetFileNameWithoutExtension(item.PathName);
+                        rbmp.Save(label4.Text + @"\" + filename + ext, imagetype);
+                        Invoke(new SetStatusDelegate(SetStatus), new Object[] { item, false });
+                    }
+                    Invoke(new SetProgressBarValueDelegate(SetProgressBarValue), new Object[] { i + 1 });
+                }
+            }
+            catch (System.Threading.ThreadAbortException e)
+            {
+                res = "Abort.";
+            }
+            finally
+            {
+                Invoke(new SetButtonTextDelegate(SetButtonText), new Object[] { "Convert" });
+                MessageBox.Show(res, "");
+            }
+
+        }
+
+    }
+
+
+
+    public class ImageRow : ListViewItem
+    {
+        public string PathName;
+
+        public ImageRow(string path)
+            : base(Path.GetFileName(path))
+        {
+            Checked = true;
+            PathName = path;
+            Image im = Image.FromFile(path);
+
+            SubItems.Add(im.Width.ToString());
+            SubItems.Add(im.Height.ToString());
+        }
+    }
+
+}
